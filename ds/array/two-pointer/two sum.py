2d5602c3472d181de9de3f639d199271d9463ac6
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2024-06-05 08:04:14 +0530

    Create two sum.py

---
+++ ds/array/two-pointer/two sum.py
@@ -0,0 +1,12 @@
+"""
+https://leetcode.com/problems/two-sum/description/
+"""
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dp = {}
+        for i in range(0,len(nums)):
+            if target - nums[i] in dp.keys():
+                return [dp[target - nums[i]], i]
+            dp[nums[i]] = i
+        return []
