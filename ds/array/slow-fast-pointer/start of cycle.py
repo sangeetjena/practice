42c69f250df7ab51e75d4b09625d37136d4e1ef5
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2024-06-10 08:24:52 +0530

    Create start of cycle.py

---
+++ ds/array/slow-fast-pointer/start of cycle.py
@@ -0,0 +1,26 @@
+"""
+https://leetcode.com/problems/linked-list-cycle-ii/submissions/1283344582/
+https://www.youtube.com/watch?v=95ZfuoSAUPI
+"""
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+        while fast!=None and fast.next!=None:
+            fast=fast.next.next
+            slow = slow.next
+
+            if fast == slow:
+                cnt=0
+                while slow != head:
+                    slow = slow.next
+                    head = head.next
+                    cnt+=1
+                return head
+        return None
+        
