37ed42b4ff5c5303d01edc48e531a8ccfa96b392
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2024-06-10 08:54:11 +0530

    Create pallindromic_linkedlist.py

---
+++ ds/array/slow-fast-pointer/pallindromic_linkedlist.py
@@ -0,0 +1,29 @@
+"""
+https://leetcode.com/problems/palindrome-linked-list/
+https://www.youtube.com/watch?v=yOzXms1J6Nk
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        fast = slow = head
+        while fast!=None and fast.next!=None:
+            fast=fast.next.next
+            slow=slow.next
+        prev = None
+        while slow:
+            temp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = temp
+        while prev:
+            if head.val != prev.val:
+                return False
+            prev=prev.next
+            head=head.next
+        return True
+        
