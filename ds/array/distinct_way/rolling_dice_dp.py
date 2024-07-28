ee78b49cd308d11dd83d30c1738a9bbe1e77fa63
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2024-07-28 10:00:35 +0530

    Update rolling_dice_dp.py

--- ds/array/distinct_way/rolling_dice_dp.py
+++ ds/array/distinct_way/rolling_dice_dp.py
@@ -1,6 +1,19 @@
 """
 https://leetcode.com/problems/number-of-dice-rolls-with-target-sum/
 https://www.youtube.com/watch?v=hfUxjdjVQN4&t=1s
+
+You have n dice, and each dice has k faces numbered from 1 to k.
+
+Given three integers n, k, and target, return the number of possible ways (out of the kn total ways) to roll the dice, so the sum of the face-up numbers equals target. Since the answer may be too large, return it modulo 109 + 7.
+
+ 
+
+Example 1:
+
+Input: n = 1, k = 6, target = 3
+Output: 1
+Explanation: You throw one die with 6 faces.
+There is only one way to get a sum of 3.
 """
 class Solution:
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
