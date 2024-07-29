a76ab28cbd6067584bf07ffcf60c30fb4223553d
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2024-07-29 08:53:59 +0530

    Update unique_binary_search_tree.py

--- ds/array/merging_interval/unique_binary_search_tree.py
+++ ds/array/merging_interval/unique_binary_search_tree.py
@@ -1,5 +1,9 @@
 """
 https://leetcode.com/problems/unique-binary-search-trees/submissions/1268231700/
+here value of nodes doesnot matter, it depends if you take a value as root how many elements are there in its left and its right.
+that will decide how many tree can be formed.
+1st take small window and check if we will take one value in the windlow how many tree can beformed, store that to the dp 
+then increase the window size and reuse already calculated value for its earlier window.
 """
 class Solution:
     def numTrees(self, n: int) -> int:
