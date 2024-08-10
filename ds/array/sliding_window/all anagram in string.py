ed63a4170a4b770faee699cb9346f86553681da9
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2024-08-10 11:47:33 +0530

    Update all anagram in string.py

--- ds/array/sliding_window/all anagram in string.py
+++ ds/array/sliding_window/all anagram in string.py
@@ -1,6 +1,22 @@
 """
 https://leetcode.com/problems/find-all-anagrams-in-a-string/description/
 
+Note: find count of all charector in the one string and check if the window you are taking in other string , char count exactly match with the other strin, then it is an anagram.
+
+Given two strings s and p, return an array of all the start indices of p's anagrams in s. You may return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: s = "cbaebabacd", p = "abc"
+Output: [0,6]
+Explanation:
+The substring with start index = 0 is "cba", which is an anagram of "abc".
+The substring with start index = 6 is "bac", which is an anagram of "abc".
+
 """
 
 class Solution:
