39b3d23e51ab8156ccf8c6a3635516bf5dd22465
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2024-08-20 08:10:26 +0530

    Update cheat.sql

--- sql/cheat.sql
+++ sql/cheat.sql
@@ -12,3 +12,22 @@ Date format:
 ===========================
 DATE_FORMAT(created_at, '%Y-%m') = '2020-02'
 date_sub(dt, interval rnk day)
+
+
+Recurrsive query:
+===========================
+wtih [RECURRSIVE] cte as (
+  select [ base query]
+  union all
+  recurrsive query from cte [with condition to break the loop]
+)
+select from cte
+
+ex: print numbers from 1 to 10:
+with recurrsive cte as (
+  select 1 as num
+    union all
+  select num +1
+    from cte where num <10
+  )
+select * form cte
