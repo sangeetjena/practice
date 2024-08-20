e9c7ef2f77eedd94cde721d19296cb03f6311350
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2024-08-20 07:57:12 +0530

    Update cheat.sql

--- sql/cheat.sql
+++ sql/cheat.sql
@@ -2,3 +2,13 @@
 analytical functions:
 ============================
 SUM(salary) OVER (PARTITION BY id ORDER BY month RANGE BETWEEN 2 PRECEDING AND CURRENT ROW) AS Salary
+sum(weight)over(order by turn asc rows between unbounded preceding and current row )
+rank()over(order by total_weight desc)
+
+
+
+
+Date format:
+===========================
+DATE_FORMAT(created_at, '%Y-%m') = '2020-02'
+date_sub(dt, interval rnk day)
