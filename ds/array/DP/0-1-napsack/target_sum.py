602376b883ea2a61623ffec7fc1dc3bdc03e5b07
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2024-09-01 12:31:39 +0530

    Update target_sum.py

--- ds/array/DP/0-1-napsack/target_sum.py
+++ ds/array/DP/0-1-napsack/target_sum.py
@@ -31,3 +31,38 @@ class Solution:
                     dp[i][j] = dp[i-1][j]
         print(dp)
         return dp[-1][-1]
+
+# Memoisation:
+=============
+def find_target_sum_ways(arr, T):
+    total = sum(arr)
+
+    # If the target can't be generated using the given numbers
+    if total < abs(T):
+        return 0
+    
+    # Initialize a lookup table
+    dp = [[-1 for _ in range(2*total+1)] for _ in range(len(arr))]
+    
+    return find_target_sum_ways_rec(arr, 0, T, dp)
+
+def find_target_sum_ways_rec(arr, i, T, dp):
+    # If all integers are processed
+    if i == len(arr):
+        # If target is reached
+        if T == 0:
+            return 1
+        # If target is not reached
+        return 0
+
+    #If we have solved it earlier, then return the result from memory
+    if dp[i][T] != -1:
+        return dp[i][T]
+    
+    # Calculate both sub-problems and save the results in the memory
+    dp[i][T] = find_target_sum_ways_rec(arr, i + 1, T + arr[i], dp) + \
+               find_target_sum_ways_rec(arr, i + 1, T - arr[i], dp)
+    
+    return dp[i][T]
+
+
