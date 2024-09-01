b41ea3b534e5bb6745984b4ebdf40655825dfdd6
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2024-09-01 12:30:12 +0530

    Update target_sum.py

--- ds/array/DP/0-1-napsack/target_sum.py
+++ ds/array/DP/0-1-napsack/target_sum.py
@@ -11,20 +11,23 @@ now the problem became subset sum equals to sum1
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         total = sum(nums)
-        sm1 = (total+target)//2
-        # if i have all +ve no and sum is  < target value then we can't build partition in any way.
-        if target> total:
+        # if i have only one element and that one element match to target then there is only one way i canform the target
+        if len(nums) == 1:
+            return 1 if (nums[0] == target) or (-nums[0] == target) else 0
+        if (total+target)%2 != 0:
             return 0
+        # if i have all +ve no and sum is  < target value then we can't build partition in any way.
+        if abs(target)> total:
+            return 0
+        sm1 = (total+target)//2
+        
         dp = [[0 for i in range(sm1+1)] for j in range(len(nums)+1)]
-        for i in range(len(nums)+1):
+        dp[0][0]=1
+        for i in range(1,len(nums)+1):
             for j in range(sm1+1):
-                if j == 0:
-                    dp[i][j]=1
-                    continue
                 if nums[i-1] <= j:
                     dp[i][j]= dp[i-1][j] + dp[i-1][j-nums[i-1]]
                 else:
                     dp[i][j] = dp[i-1][j]
         print(dp)
         return dp[-1][-1]
-        
