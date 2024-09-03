5b377de03992b16c324fee2b9ca1de7ad360d563
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2024-09-03 08:00:25 +0530

    Create diff_way_to_express_num.py

---
+++ ds/array/DP/Fibonacci-pattern/diff_way_to_express_num.py
@@ -0,0 +1,22 @@
+"""
+
+
+"""
+def countWays(n): 
+  
+    DP = [0 for i in range(0, n + 1)] 
+      
+    # base cases 
+    DP[0] = DP[1] = DP[2] = 1
+    DP[3] = 2
+  
+    # Iterate for all values from 4 to n 
+    for i in range(4, n + 1): 
+        DP[i] = DP[i - 1] + DP[i - 3] + DP[i - 4] 
+      
+    return DP[n] 
+  
+      
+# Driver code  
+n = 10
+print (countWays(n)) 
