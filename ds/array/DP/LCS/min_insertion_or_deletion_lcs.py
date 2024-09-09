87088837cf9337c08bbd6ed485815eda95056ed9
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2024-09-09 11:19:56 +0530

    Update min_insertion_or_deletion_lcs.py

--- ds/array/DP/LCS/min_insertion_or_deletion_lcs.py
+++ ds/array/DP/LCS/min_insertion_or_deletion_lcs.py
@@ -2,5 +2,5 @@
 Minimum Deletions & Insertions to Transform a String into another
 
 https://practice.geeksforgeeks.org/problems/minimum-number-of-deletions-and-insertions0209/1/
-
+Note: similar to https://github.com/sangeetjena/practice/blob/master/ds/array/DP/LCS/Edit_distance.py only only remove replace condition.
 """
