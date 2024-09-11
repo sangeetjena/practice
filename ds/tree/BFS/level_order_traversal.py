4397a30cd04da12c720e1c90209dac7c37b116de
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2024-09-11 08:07:02 +0530

    Update level_order_traversal.py

--- ds/tree/BFS/level_order_traversal.py
+++ ds/tree/BFS/level_order_traversal.py
@@ -1,1 +1,37 @@
+"""
+https://leetcode.com/problems/binary-tree-level-order-traversal/
 
+"""
+
+from collections import deque
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return None
+            
+        levels = [] # List of Lists
+        queue = deque( [root] ) # At 1st, Placing Root Node into the Queue
+
+        while queue: # Traversing through Queue
+            # Getting Current Level Size
+            cur_level_size = len(queue)
+            cur_level = [] # List to Stores Val's of Current Level
+            
+            for _ in range(cur_level_size):
+                # Take the Front Guy
+                front = queue.popleft()
+                 
+                # Store Front Node in Current Level List
+                cur_level.append(front.val)
+
+                # Appending the Left & Right Child of Front Node, If Exist
+                if front.left:
+                    queue.append(front.left)
+                if front.right:
+                    queue.append(front.right)
+            
+            # After While Loop, cur_level(List) contains Values of One Level
+            # So, Appending the List in Answer List
+            levels.append(cur_level)
+
+        return levels
