fcedfd0f11b49653fcf49e446a0fb9aa26064f45
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2024-09-12 22:20:47 +0530

    Create path_sum.py

---
+++ ds/tree/DFS/path_sum.py
@@ -0,0 +1,39 @@
+"""
+https://leetcode.com/problems/path-sum/description/
+
+Step1: add element to dfs and add current node value to the sum
+step 2: if node visited remove fromdfs and substract from sum
+step 3: if leaf node and sum== target retrun true
+else false
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if root == None:
+            return False
+        dfs = []
+        visited = []
+        sm = 0
+        dfs.append(root)
+        while len(dfs)>0:
+            node = dfs[-1]
+            if node in visited:
+                del dfs[-1]
+                sm-=node.val
+                continue
+            sm+=node.val
+            if sm == targetSum and node.left is None and node.right is None:
+                return True
+            if node.left:
+                dfs.append(node.left)
+            if node.right:
+                dfs.append(node.right)
+            visited.append(node)
+        return False
+        
