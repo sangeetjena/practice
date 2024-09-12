c0e31b12b7691c926b74428f8e9e77b9b594b0fe
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2024-09-12 11:06:29 +0530

    Create min_depth_tree.py

---
+++ ds/tree/BFS/min_depth_tree.py
@@ -0,0 +1,36 @@
+"""
+https://leetcode.com/problems/minimum-depth-of-binary-tree/description/
+
+Given a binary tree, find its minimum depth.
+
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+Note: A leaf is a node with no children.
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if root==None:
+            return 0
+        queue = deque([root])
+        level = 1
+        while len(queue)>0:
+            q_len = len(queue)
+            for i in range(q_len):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                if node.left==None and node.right==None:
+                    return level
+            level+=1
+        return level
+
+        
