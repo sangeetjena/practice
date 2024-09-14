c9db57046d98025e2adf93ecbe79a35b61cf9d5e
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2024-09-14 15:39:09 +0530

    Update max_diameter.py

--- ds/tree/DFS/max_diameter.py
+++ ds/tree/DFS/max_diameter.py
@@ -4,8 +4,8 @@ https://leetcode.com/problems/diameter-of-binary-tree/
 Note: for each node while returning to it parent node return the max diameter sofar and max defth from current node.
 on root node check if max diameter is greater than current diameter then return max diameter.
 """
-
-
+        
+        
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -13,19 +13,18 @@ on root node check if max diameter is greater than current diameter then return 
 #         self.left = left
 #         self.right = right
 class Solution:
-    def dfs(self, node, root, maxDiameter):
+    def dfs(self, node, dp):
         if node == None:
-            return 0,0
-        lmaxDiameter, left = self.dfs(node.left, root, maxDiameter) 
-        rmaxDiameter, right = self.dfs(node.right, root, maxDiameter)
-        maxDiameter = max(lmaxDiameter, rmaxDiameter, maxDiameter)
-        if node == root:
-            # at the root if in its child( left and right) branch  max diameter would have formed then return that.
-            return max(maxDiameter,left+right)
-        else:
-            # max condition to check if this would be the root node then what would be the max diameter 
-            return  max(maxDiameter,left+right), 1 + max(left, right)
+            return 0
+        left = self.dfs(node.left, dp) 
+        right = self.dfs(node.right, dp)
+        # what is the max diameter at this node
+        dp[0] = max(dp[0],left+right)
+        # return to parent node what is the max depth sofar
+        return 1 + max(left, right)
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        return self.dfs(root, root,0)
+        dp = [0]
+        self.dfs(root, dp)
+        return dp[0]
         
         
