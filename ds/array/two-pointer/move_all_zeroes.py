284623f2711200c298f6a53b2f76f8ba78c5af46
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2024-09-16 19:48:46 +0530

    Update move_all_zeroes.py

--- ds/array/two-pointer/move_all_zeroes.py
+++ ds/array/two-pointer/move_all_zeroes.py
@@ -23,11 +23,10 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         i=j=0
-        while i<len(nums) and j< len(nums):
-            if nums[j] == 0:
-                j+=1
-            else:
-                nums[i], nums[j] = nums[j],nums[i]
+        while j< len(nums):
+            # keep fix i if get 0 else increment i , for j keep on incresing.
+            if nums[i]==0 and nums[j]!=0:
+                nums[i], nums[j] = nums[j], nums[i]
+            if nums[i]!=0:
                 i+=1
-                j+=1
-        return nums
+            j+=1
