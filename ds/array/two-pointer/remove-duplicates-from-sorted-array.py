b0f27e27780c05011cf92c560da908b88ff98e47
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2024-09-16 19:33:30 +0530

    Update remove-duplicates-from-sorted-array.py

--- ds/array/two-pointer/remove-duplicates-from-sorted-array.py
+++ ds/array/two-pointer/remove-duplicates-from-sorted-array.py
@@ -1,6 +1,26 @@
 """
 https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+
+Note: # keep two pointer 
+# j pointer increment till get the the 1st non duplicate element and keep i pointer on last swap location
 """
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i,j = 1,1
+        # keep two pointer 
+        # j pointer increment till get the the 1st non duplicate element and keep i pointer on last swap location
+        while j< len(nums):
+            if nums[j] == nums[j-1]:
+                j+=1
+                continue
+            else:
+                nums[i] =  nums[j]
+                j+=1
+                i+=1
+        return i
+
+==========================================
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
