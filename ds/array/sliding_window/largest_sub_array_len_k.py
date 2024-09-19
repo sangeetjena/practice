ef394c05ac4fdce2e26651d412ab0fd42ecf88ed
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2024-09-19 09:11:54 +0530

    Create largest_sub_array_len_k.py

---
+++ ds/array/sliding_window/largest_sub_array_len_k.py
@@ -0,0 +1,42 @@
+"""
+https://leetcode.com/problems/largest-subarray-length-k/
+
+An array A is larger than some array B if for the first index i where A[i] != B[i], A[i] > B[i].
+
+For example, consider 0-indexing:
+
+[1,3,2,4] > [1,2,2,4], since at index 1, 3 > 2.
+[1,4,4,4] < [2,1,1,1], since at index 0, 1 < 2.
+A subarray is a contiguous subsequence of the array.
+
+Given an integer array nums of distinct integers, return the largest subarray of nums of length k.
+
+ 
+
+Example 1:
+
+Input: nums = [1,4,5,2,3], k = 3
+Output: [5,2,3]
+Explanation: The subarrays of size 3 are: [1,4,5], [4,5,2], and [5,2,3].
+Of these, [5,2,3] is the largest.
+
+
+"""
+class Solution:
+    def largestSubarray(self, nums: List[int], k: int) -> List[int]:
+        mx = 0
+        res = []
+        for i in range(len(nums)-k+1):
+            tmp = nums[i:i+k]
+            if len(res) == 0:
+                res = tmp
+                continue
+            for i in range(k):
+                if res[i] > tmp[i]:
+                    break
+                if res[i] < tmp[i]:
+                    res = tmp
+                    break
+        return res
+
+        
