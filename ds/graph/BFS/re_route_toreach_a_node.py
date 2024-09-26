d767d9db7d6a67bd9d746f264f7b7daa966f258f
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2024-09-26 10:05:41 +0530

    Create re_route_toreach_a_node.py

---
+++ ds/graph/BFS/re_route_toreach_a_node.py
@@ -0,0 +1,5 @@
+"""
+https://leetcode.com/problems/reorder-routes-to-make-all-paths-lead-to-the-city-zero/description/
+
+Note: create hashmap and convert graph to undirected graph. then check all child node is coming inword or not and count outward childs.
+"""
