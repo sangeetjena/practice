4902e224a0fb921d203dc77ba8237a2236212f62
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2024-09-27 18:56:28 +0530

    Update word_ladder.py

--- ds/graph/BFS/word_ladder.py
+++ ds/graph/BFS/word_ladder.py
@@ -1,6 +1,7 @@
 """
+https://leetcode.com/problems/word-ladder/description/
 
-
+Note: find all the regex and store that as the key in dictionary , then create prefix  set and construct graph and then run bfs.
 """
 
 from collections import deque
