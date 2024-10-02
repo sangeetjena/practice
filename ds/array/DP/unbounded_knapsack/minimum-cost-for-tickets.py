8b8d225a369e008848a62eb42d6efff4a6aa642c
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2024-10-02 23:14:21 +0530

    Update minimum-cost-for-tickets.py

--- ds/array/DP/unbounded_knapsack/minimum-cost-for-tickets.py
+++ ds/array/DP/unbounded_knapsack/minimum-cost-for-tickets.py
@@ -14,7 +14,10 @@ class Solution:
             if i not in days:
                 dp[i] = dp[i-1]
             else:
-                # by substracting 1,7 or 30 days from todays visited date it will land in either one of the previous visited date or non visited date. but we have already captured for nonvisited date ( cost of previous visited date i.e dp[i] = dp[i-1]), so if it will land in any non visited date then it will pull cost of last visited date and if it land in any visited date then that is the border line date then simply take the min ticket cost on that date.
+                # by substracting 1,7 or 30 days from todays visited date it will land in either one of the previous visited date or non visited date. 
+                # but we have already captured for nonvisited date ( cost of previous visited date i.e dp[i] = dp[i-1]), 
+                # so if it will land in any non visited date then it will pull cost of last visited date and if it land in any visited date then that is the border line date then
+                # simply take the min ticket cost on that date.
                 dp[i] = min(
                     (costs[0]+ dp[max(0, i-1)]), 
                     (costs[1]+ dp[max(0, i-7)]),
