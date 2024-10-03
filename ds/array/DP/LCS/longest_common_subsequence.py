60acaeee3f333d7a4733bb0925d60b9e0ea85174
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2024-10-03 18:48:45 +0530

    Update longest_common_subsequence.py

--- ds/array/DP/LCS/longest_common_subsequence.py
+++ ds/array/DP/LCS/longest_common_subsequence.py
@@ -1,4 +1,28 @@
 """
+https://leetcode.com/problems/longest-common-subsequence/description/
 
+Q:  1143. Longest Common Subsequence
+Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
 
+A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+
+For example, "ace" is a subsequence of "abcde".
+A common subsequence of two strings is a subsequence that is common to both strings.
+
+Example 1:
+
+Input: text1 = "abcde", text2 = "ace"
+Output: 3
+Explanation: The longest common subsequence is "ace" and its length is 3.
 """
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        dp = [[0 for i in range(len(text1) + 1)] for j in range(len(text2) +1)]
+        for i in range(1, len(text1) + 1):
+            for j in range(1, len(text2) + 1):
+                if text1[i-1] == text2[j-1]:
+                    dp[j][i] = 1+ dp[j-1][i-1]
+                else:
+                    dp[j][i] = max(dp[j-1][i] , dp[j][i-1])
+        return dp[-1][-1]
+        
