6a1e24bc27b9421fd657a185b12c2f231c17ac2f
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2024-11-25 07:53:58 +0530

    Update combination_sum.py

--- ds/array/DP/0-1-napsack/combination_sum.py
+++ ds/array/DP/0-1-napsack/combination_sum.py
@@ -1,9 +1,39 @@
 """
 https://leetcode.com/problems/combination-sum/description/
-Note: can solved in backtracking . solved in subset pattern.
+Note: can solved in backtracking . solved in subset pattern. to avoid duplicate take index and always take next index >= currrent index.
 
 """
+=======0/1 knap sack =====
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        dp = defaultdict(list)
+        candidates.sort()
+        for c in candidates:
+            dp[c].append([c])
+        print(dp)
+        for i in range(target+1):
+            for j in candidates:
+                if j <= i:
+                    for elem in dp[i-j]:
+                        print("for "+str(i)+"--"+ str(elem))
+                        if elem == None:
+                            continue
+                        if elem[-1]> j:
+                            continue
+                        val = copy.deepcopy(elem)
+                        val.append(j)
+                        dp[i].append(val)
+                        print("for "+str(i)+"--"+ str(dp[i]), str(val))
+        print(dp)
+        return dp[target]
 
+
+
+        
+
+
+
+======back tracking =====
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         total_list = []
