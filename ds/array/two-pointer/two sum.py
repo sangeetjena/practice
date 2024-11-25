9cfc7a6b655147bf4e3b84c322dffa0eea0b26f7
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2024-11-25 11:22:54 +0530

    Update two sum.py

--- ds/array/two-pointer/two sum.py
+++ ds/array/two-pointer/two sum.py
@@ -11,3 +11,9 @@ class Solution:
                 return [dp[target - nums[i]], i]
             dp[nums[i]] = i
         return []
+
+
+======== two pointer approch ======
+# sort the list and keep one pointer at end and one at begining
+# if sum is less then move the left else right 
+# break the loop if element found 
