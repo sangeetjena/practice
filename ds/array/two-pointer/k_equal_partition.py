79838238ea5cd24e6211fcb320f17f95e13ece59
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2024-11-25 11:24:38 +0530

    Create k_equal_partition.py

---
+++ ds/array/two-pointer/k_equal_partition.py
@@ -0,0 +1,7 @@
+"""
+https://leetcode.com/problems/partition-to-k-equal-sum-subsets/description/
+
+Note: sort the list. 
+use one pointer at the higest index and use sliding window from begining
+
+"""
