2901c26e8d0d8fd03ba8db1a123a445147f99efe
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2024-12-03 22:49:14 +0530

    Update make_arr_to_zero.py

--- ds/array/merging_interval/make_arr_to_zero.py
+++ ds/array/merging_interval/make_arr_to_zero.py
@@ -7,3 +7,18 @@ Note: create difference set,
       so we can say for a subset what really change is starting element of the subset and end +1 element in the subset.
       2- after all the operation if cumilative difference eqal to the element , then the ele,ent can become 0.
 """
+class Solution:
+    def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
+        arr = [0 for i in range(len(nums))]
+        for l,r in queries:
+            arr[l]+=1
+            if r<len(nums)-1:
+                arr[r+1]-=1
+        for i in range(len(nums)):
+            if arr[i] < nums[i]:
+                return False
+            if i<len(nums)-1:
+                arr[i+1] = arr[i+1] + arr[i]
+        return True
+
+        
