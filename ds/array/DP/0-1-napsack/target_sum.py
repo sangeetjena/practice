77e9b165cac7321b3e63d83fde93fb922c4f9a92
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2025-02-01 11:39:11 +0530

    Update target_sum.py

--- ds/array/DP/0-1-napsack/target_sum.py
+++ ds/array/DP/0-1-napsack/target_sum.py
@@ -1,5 +1,25 @@
 """
 https://leetcode.com/problems/target-sum/
+You are given an integer array nums and an integer target.
+
+You want to build an expression out of nums by adding one of the symbols '+' and '-' before each integer in nums and then concatenate all the integers.
+
+For example, if nums = [2, 1], you can add a '+' before 2 and a '-' before 1 and concatenate them to build the expression "+2-1".
+Return the number of different expressions that you can build, which evaluates to target.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1,1,1], target = 3
+Output: 5
+Explanation: There are 5 ways to assign symbols to make the sum of nums be target 3.
+-1 + 1 + 1 + 1 + 1 = 3
++1 - 1 + 1 + 1 + 1 = 3
++1 + 1 - 1 + 1 + 1 = 3
++1 + 1 + 1 - 1 + 1 = 3
++1 + 1 + 1 + 1 - 1 = 3
+
 
 Note: it is similar to subset sum = k, we need to devide nums to two sets one +ve and one -ve.
 sum1 - sum2 = target # sum2 of -ve nos
