67e9455c6360548fa18cce9e61db50c4767fa69a
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2025-03-02 23:58:33 +0530

    Update valid_graph_to_tree.py

--- ds/graph/topological_sort/valid_graph_to_tree.py
+++ ds/graph/topological_sort/valid_graph_to_tree.py
@@ -1,6 +1,6 @@
 """
 https://leetcode.com/problems/graph-valid-tree/description/
 
-note: one single child will not be pointed bye, 2 parent node
+note: one single child will not be pointed by, 2 parent node
 
 """
