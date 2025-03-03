6c5dc7d07835227e9bb36277ec8a1a8e20b8e828
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2025-03-03 18:55:00 +0530

    Update valid_graph_to_tree.py

--- ds/graph/topological_sort/valid_graph_to_tree.py
+++ ds/graph/topological_sort/valid_graph_to_tree.py
@@ -4,3 +4,26 @@ https://leetcode.com/problems/graph-valid-tree/description/
 note: one single child will not be pointed by, 2 parent node
 
 """
+from collections import defaultdict
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        # we need to find if there is no cycle in the graph.
+        visited = []
+        bfs = [0]
+        graph = defaultdict(set)
+        for parent, chield in edges:
+            # for undirected graph store both ((p->c) and (c->p))
+            graph[parent].add(chield)
+            graph[chield].add(parent)
+        while bfs:
+            node = bfs[-1]
+            if node in visited:
+                return False
+            del bfs[-1]
+            for chield in graph[node]:
+                bfs.append(chield)
+                # if child already added to the dfs, remove reverse store (child storing parent information) for undirected graph (p->c) and (c->p) to avoid reprocessing same edge.
+                graph[chield].remove(node)
+            if node not in visited:
+                visited.append(node)
+        return True if len(visited) == n else False
