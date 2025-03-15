8119ab4c865e05dbaa146934b1a8453a051f9adb
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2025-03-15 11:36:07 +0530

    Update path_sum_to_target.py

--- ds/tree/DFS/path_sum_to_target.py
+++ ds/tree/DFS/path_sum_to_target.py
@@ -3,6 +3,32 @@ https://leetcode.com/problems/path-sum-iii/description/
 
 Note: similar to prefix sum ( sub array sum to target)
 """
+# optimal solution using prefix sum:
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        # Note: THis is similar to the prefix sum problem. but in tree. 
+        # the total sum will increase when traversal is in one direction, when it change the direction then, reset the sum again and cntinue. 
+        self.totalMatch = 0
+        from collections import defaultdict
+        def findTarget(root, total, prefixSum):
+            if root == None:
+                return
+            if (total+root.val) - targetSum in prefixSum.keys():
+                self.totalMatch+=prefixSum[(total+root.val)-targetSum]
+                print("found one" + str(self.totalMatch))
+            prefixSum[total + root.val]+=1
+            findTarget(root.left,total+root.val, prefixSum)
+            findTarget(root.right, total+root.val, prefixSum)
+            prefixSum[total + root.val]-=1
+        findTarget(root, 0, defaultdict(int, {0:1}))
+        return self.totalMatch
+        
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
