b08d8df1270f7daf0cf983825193b182391980bc
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2025-03-17 07:03:54 +0530

    Update online_stock_spanner.py

--- ds/array/monotonic_stack/online_stock_spanner.py
+++ ds/array/monotonic_stack/online_stock_spanner.py
@@ -1,5 +1,25 @@
 """
 https://leetcode.com/problems/online-stock-span/description/?envType=study-plan-v2&envId=leetcode-75
+Design an algorithm that collects daily price quotes for some stock and returns the span of that stock's price for the current day.
+
+The span of the stock's price in one day is the maximum number of consecutive days (starting from that day and going backward) for which the stock price was less than or equal to the price of that day.
+
+For example, if the prices of the stock in the last four days is [7,2,1,2] and the price of the stock today is 2, then the span of today is 4 because starting from today, the price of the stock was less than or equal 2 for 4 consecutive days.
+Also, if the prices of the stock in the last four days is [7,34,1,2] and the price of the stock today is 8, then the span of today is 3 because starting from today, the price of the stock was less than or equal 8 for 3 consecutive days.
+Implement the StockSpanner class:
+
+StockSpanner() Initializes the object of the class.
+int next(int price) Returns the span of the stock's price given that today's price is price.
+ 
+
+Example 1:
+
+Input
+["StockSpanner", "next", "next", "next", "next", "next", "next", "next"]
+[[], [100], [80], [60], [70], [60], [75], [85]]
+Output
+[null, 1, 1, 1, 2, 1, 4, 6]
+
 
 Note: monotonic stack pattern. when find a value greater than the values in teh stack evalute all the value and 
 insert the new value along with total count because of new value.
