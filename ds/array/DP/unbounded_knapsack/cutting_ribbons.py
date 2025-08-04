0e0f336a3d0b0b68db1756cba4808a0bcff3aa31
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2025-08-04 08:43:28 +0530

    Update cutting_ribbons.py

--- ds/array/DP/unbounded_knapsack/cutting_ribbons.py
+++ ds/array/DP/unbounded_knapsack/cutting_ribbons.py
@@ -1,5 +1,27 @@
 """
 https://leetcode.com/problems/cutting-ribbons/description/
+
+You are given an integer array ribbons, where ribbons[i] represents the length of the ith ribbon, and an integer k. You may cut any of the ribbons into any number of segments of positive integer lengths, or perform no cuts at all.
+
+For example, if you have a ribbon of length 4, you can:
+Keep the ribbon of length 4,
+Cut it into one ribbon of length 3 and one ribbon of length 1,
+Cut it into two ribbons of length 2,
+Cut it into one ribbon of length 2 and two ribbons of length 1, or
+Cut it into four ribbons of length 1.
+Your task is to determine the maximum length of ribbon, x, that allows you to cut at least k ribbons, each of length x. You can discard any leftover ribbon from the cuts. If it is impossible to cut k ribbons of the same length, return 0.
+
+ 
+
+Example 1:
+
+Input: ribbons = [9,7,5], k = 3
+Output: 5
+Explanation:
+- Cut the first ribbon to two ribbons, one of length 5 and one of length 4.
+- Cut the second ribbon to two ribbons, one of length 5 and one of length 2.
+- Keep the third ribbon as it is.
+Now you have 3 ribbons of length 5.
 Note: simple solution is to increment k value from 1 to max(ribbons) but to optimise lienear search use binary search
 
 """
