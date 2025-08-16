5107ee6e38cef1093840c27d52c2254a821dd41d
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2025-08-16 22:01:12 +0530

    Update burst_baloon.py

--- ds/array/DP/Matrix_Chain_Multiplicaton/burst_baloon.py
+++ ds/array/DP/Matrix_Chain_Multiplicaton/burst_baloon.py
@@ -1,26 +1,47 @@
 """
-Note: be carefull while using right index, it might go index out of range.
+312. Burst Balloons
+Solved
+Hard
+Topics
+conpanies icon
+Companies
+You are given n balloons, indexed from 0 to n - 1. Each balloon is painted with a number on it represented by an array nums. You are asked to burst all the balloons.
+
+If you burst the ith balloon, you will get nums[i - 1] * nums[i] * nums[i + 1] coins. If i - 1 or i + 1 goes out of bounds of the array, then treat it as if there is a balloon with a 1 painted on it.
+
+Return the maximum coins you can collect by bursting the balloons wisely.
+
+ 
+
+Example 1:
+
+Input: nums = [3,1,5,8]
+Output: 167
+Explanation:
+nums = [3,1,5,8] --> [3,5,8] --> [3,8] --> [8] --> []
+coins =  3*1*5    +   3*5*8   +  1*3*8  + 1*8*1 = 167
+Example 2:
+
+Input: nums = [1,5]
+Output: 10
+
 """
-def burst_baloon(arr):
-    arr = [1] + arr + [1]
-    dp = [[0 for i in range(len(arr))] for x in range(len(arr))]
-    for window in range(1, len(arr) - 1):
-        for i in range(1, len(arr) - window):
-            left = i
-            right = i + window - 1
-            # print("dp left right")
-            # print(left, right, window)
-            for j in range(left, right +1):
-                # print(j)
-                dp[left][right] = max(dp[left][right],
-                                      arr[j] * arr[left - 1] * arr[right + 1] +
-                                      dp[left][j - 1] +
-                                      dp[j + 1][right])
-            #     print(arr[j] , arr[left - 1] , arr[right + 1] )
-            #     print(dp[left][j - 1], dp[j + 1][right])
-            # print(dp)
-    print(dp[1][-2])
-    return dp
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+        n=len(nums)
+        nums.insert(0,1)
+        nums.append(1)
+        dp=[[0 for j in range(n+2)] for j in range(n+2)]
+        for i in range(n,0,-1):
+            for j in range(1,n+1):
+                if i>j:
+                    continue
+                maxi=-maxsize
+                for k in range(i,j+1):
+                    coins=nums[i-1]*nums[k]*nums[j+1]+dp[i][k-1]+dp[k+1][j]
+                    maxi=max(maxi,coins)
+                dp[i][j]=maxi
+        return dp[1][n]
 
 
 arr = [1, 2, 3, 4]
