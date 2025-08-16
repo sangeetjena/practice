705351ff2b200dffa65332df82e7acea4dc5d0d5
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2025-08-16 14:55:39 +0530

    Update Edit_distance.py

--- ds/array/DP/LCS/Edit_distance.py
+++ ds/array/DP/LCS/Edit_distance.py
@@ -22,7 +22,11 @@ class Solution:
                 else:
                     # if not match then option 1: delete or insert in word1 or word2 i.e (dp[i-1][j] or dp[i][j-1])
                     # or replace charector between word1 and word2, if relaced i.e 1 operation and then bothe the word became same so check what was max operation to the previous index i.e [i-1][j-1]
-                    dp[i][j] = 1 + min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
+                    dp[i][j] = 1 + min(
+                    dp(i - 1, j),      # Delete
+                    dp(i, j - 1),      # Insert
+                    dp(i - 1, j - 1)   # Replace
+                )
         lcs = dp[-1][-1]
         print(dp)
         return lcs
