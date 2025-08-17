a6b469fe27983229cf53483ca0c6c5c3b9d25f73
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2025-08-17 11:44:45 +0530

    Update burst_baloon.py

--- ds/array/DP/Matrix_Chain_Multiplicaton/burst_baloon.py
+++ ds/array/DP/Matrix_Chain_Multiplicaton/burst_baloon.py
@@ -29,12 +29,12 @@ class Solution:
         nums.insert(0,1)
         nums.append(1)
         dp=[[0 for j in range(n+2)] for j in range(n+2)]
-        for i in range(n,0,-1):
-            for j in range(1,n+1):
+        for i in range(n,0,-1): # index position of one element in the array 
+            for j in range(1,n+1): # shifting window
                 if i>j:
                     continue
                 maxi=-maxsize
-                for k in range(i,j+1):
+                for k in range(i,j+1): # define window size
                     coins=nums[i-1]*nums[k]*nums[j+1]+dp[i][k-1]+dp[k+1][j]
                     maxi=max(maxi,coins)
                 dp[i][j]=maxi
