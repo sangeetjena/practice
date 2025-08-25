2f09af389a83535e81ea39b02ac150ca85736482
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2025-08-25 23:24:26 +0530

    Create longest_arithmatic_subsequence.py

---
+++ ds/array/DP/LIS/longest_arithmatic_subsequence.py
@@ -0,0 +1,46 @@
+"""
+https://leetcode.com/problems/longest-arithmetic-subsequence/description/
+
+Given an array nums of integers, return the length of the longest arithmetic subsequence in nums.
+
+Note that:
+
+A subsequence is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements.
+A sequence seq is arithmetic if seq[i + 1] - seq[i] are all the same value (for 0 <= i < seq.length - 1).
+ 
+
+Example 1:
+
+Input: nums = [3,6,9,12]
+Output: 4
+Explanation:  The whole array is an arithmetic sequence with steps of length = 3.
+Example 2:
+
+Input: nums = [9,4,7,2,10]
+Output: 3
+Explanation:  The longest arithmetic subsequence is [4,7,10].
+Example 3:
+
+Input: nums = [20,1,15,3,10,5,8]
+Output: 4
+Explanation:  The longest arithmetic subsequence is [20,15,10,5].
+
+Note:
+https://www.youtube.com/watch?v=_xIkHvSZpDE
+"""
+class Solution:
+    def longestArithSeqLength(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n <= 2:
+            return n
+
+        longest = 2
+        dp = [{} for _ in range(n)]
+
+        for i in range(n):
+            for j in range(i):
+                diff = nums[i] - nums[j]
+                dp[i][diff] = dp[j].get(diff, 1) + 1
+                longest = max(longest, dp[i][diff])
+
+        return longest
