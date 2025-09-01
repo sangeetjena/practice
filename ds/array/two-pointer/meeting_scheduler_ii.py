3d1e46306747b7ff9b9516c6f5f5752cc2840ba6
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2025-09-01 09:12:00 +0530

    Update meeting_scheduler_ii.py

--- ds/array/two-pointer/meeting_scheduler_ii.py
+++ ds/array/two-pointer/meeting_scheduler_ii.py
@@ -1,6 +1,20 @@
 """
 https://leetcode.com/problems/meeting-rooms-ii/description/?envType=problem-list-v2&envId=two-pointers
 
+Given an array of meeting time intervals intervals where intervals[i] = [starti, endi], return the minimum number of conference rooms required.
+
+ 
+
+Example 1:
+
+Input: intervals = [[0,30],[5,10],[15,20]]
+Output: 2
+Example 2:
+
+Input: intervals = [[7,10],[2,4]]
+Output: 1
+
+
 Note:
 # this is same as platform problem 
 # sort the array take arrival and departure 
