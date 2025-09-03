034dc2752e238a046bd1510d28ac324e1f739b7d
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2025-09-03 07:03:11 +0530

    Update Permutation in String.py

--- ds/array/sliding_window/Permutation in String.py
+++ ds/array/sliding_window/Permutation in String.py
@@ -1,6 +1,23 @@
 """
 https://leetcode.com/problems/permutation-in-string/description/
 
+Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+
+In other words, return true if one of s1's permutations is the substring of s2.
+
+ 
+
+Example 1:
+
+Input: s1 = "ab", s2 = "eidbaooo"
+Output: true
+Explanation: s2 contains one permutation of s1 ("ba").
+Example 2:
+
+Input: s1 = "ab", s2 = "eidboaoo"
+Output: false
+
+
 Create a HashMap to calculate the frequencies of all characters in the pattern.
 Iterate through the string, adding one character at a time in the sliding window.
 If the character being added matches a character in the HashMap, decrement its frequency in the map. If the character frequency becomes zero, we got a complete match.
