0288c40a431edd35d5d3ea399b6f11a0ca4d8201
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2025-10-02 22:31:47 +0530

    Update k_pair_with_smallest_sum.py

--- ds/array/k-way-merge/k_pair_with_smallest_sum.py
+++ ds/array/k-way-merge/k_pair_with_smallest_sum.py
@@ -1,5 +1,25 @@
 """
 https://leetcode.com/problems/find-k-pairs-with-smallest-sums/description/
+You are given two integer arrays nums1 and nums2 sorted in non-decreasing order and an integer k.
+
+Define a pair (u, v) which consists of one element from the first array and one element from the second array.
+
+Return the k pairs (u1, v1), (u2, v2), ..., (uk, vk) with the smallest sums.
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,7,11], nums2 = [2,4,6], k = 3
+Output: [[1,2],[1,4],[1,6]]
+Explanation: The first 3 pairs are returned from the sequence: [1,2],[1,4],[1,6],[7,2],[7,4],[11,2],[7,6],[11,4],[11,6]
+Example 2:
+
+Input: nums1 = [1,1,2], nums2 = [1,2,3], k = 2
+Output: [[1,1],[1,1]]
+Explanation: The first 2 pairs are returned from the sequence: [1,1],[1,1],[1,2],[2,1],[1,2],[2,2],[1,3],[1,3],[2,3]
+
+ 
 Note: add all combination with 1st array then pop smallest element and keep adding next element from next array
 """
 import heapq
