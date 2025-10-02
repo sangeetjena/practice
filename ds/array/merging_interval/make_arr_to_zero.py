db6126ca17f0632ff7b32cb99fd4c6ce31ec599a
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2025-10-02 14:21:47 +0530

    Update make_arr_to_zero.py

--- ds/array/merging_interval/make_arr_to_zero.py
+++ ds/array/merging_interval/make_arr_to_zero.py
@@ -2,7 +2,7 @@
 https://leetcode.com/problems/zero-array-transformation-i/description/?envType=company&envId=google&favoriteSlug=google-thirty-days
 
 Solution:  https://www.youtube.com/watch?v=hOEg26zHlco
-Note: create difference set, 
+Note: create difference set, same as curporate ticket booking.(add oposite weightabe at the end of index range)
       after increament value for a sub set all the difference in value of the subset will remain same, 
       so we can say for a subset what really change is starting element of the subset and end +1 element in the subset.
       2- after all the operation if cumilative difference eqal to the element , then the ele,ent can become 0.
