9be793dff19a43f299c6027cc7e0826e42b5dea5
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2025-10-30 10:52:43 +0530

    Update decode_ways.py

--- ds/array/DP/Fibonacci-pattern/decode_ways.py
+++ ds/array/DP/Fibonacci-pattern/decode_ways.py
@@ -1,6 +1,22 @@
 """
 https://leetcode.com/problems/decode-ways/description/
 
+You have intercepted a secret message encoded as a string of numbers. The message is decoded via the following mapping:
+"1" -> 'A'
+"2" -> 'B'
+...
+"25" -> 'Y'
+"26" -> 'Z'
+However, while decoding the message, you realize that there are many different ways you can decode the message because some codes are contained in other codes ("2" and "5" vs "25").
+
+For example, "11106" can be decoded into:
+"AAJF" with the grouping (1, 1, 10, 6)
+"KJF" with the grouping (11, 10, 6)
+The grouping (1, 11, 06) is invalid because "06" is not a valid code (only "6" is valid).
+Note: there may be strings that are impossible to decode.
+Given a string s containing only digits, return the number of ways to decode it. If the entire string cannot be decoded in any valid way, return 0.
+The test cases are generated so that the answer fits in a 32-bit integer.
+
 Note:
 1. Check if the input string s is empty or starts with '0'. If so, return 0 because a valid decoding is not possible.
 2. Initialize a dynamic programming array dp of size n + 1, where n is the length of the input string. Set dp[0] and dp[1] to 1, as there is one way to decode an empty string and a string of length 1.
