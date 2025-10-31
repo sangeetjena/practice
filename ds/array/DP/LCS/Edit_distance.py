2d6f458b0cb6bf2c48bd667d8b4021f72a052f93
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2025-10-31 11:36:23 +0530

    Update Edit_distance.py

--- ds/array/DP/LCS/Edit_distance.py
+++ ds/array/DP/LCS/Edit_distance.py
@@ -3,6 +3,32 @@ https://leetcode.com/problems/edit-distance/description/
 
 https://www.youtube.com/watch?v=XYi2-LPrwm4
 
+
+Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
+You have the following three operations permitted on a word:
+
+Insert a character
+Delete a character
+Replace a character
+ 
+Example 1:
+Input: word1 = "horse", word2 = "ros"
+Output: 3
+Explanation: 
+horse -> rorse (replace 'h' with 'r')
+rorse -> rose (remove 'r')
+rose -> ros (remove 'e')
+Example 2:
+
+Input: word1 = "intention", word2 = "execution"
+Output: 5
+Explanation: 
+intention -> inention (remove 't')
+inention -> enention (replace 'i' with 'e')
+enention -> exention (replace 'n' with 'x')
+exention -> exection (replace 'n' with 'c')
+exection -> execution (insert 'u')
+
 Note: similar to the LCS
 only difference is if match then don't need to increase just check if precious elemnts had anything to relace, delete . i.e dp[i-1][j-1]
 and if not match then check digonal, up, and down to check if any replace , delete or insert. 1 + min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
