b0ca0309f8d02097ce1d2b139fa20cd89ea0ae56
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2025-11-02 08:04:23 +0530

    Update longest_arithmatic_subsequence.py

--- ds/array/DP/LIS/longest_arithmatic_subsequence.py
+++ ds/array/DP/LIS/longest_arithmatic_subsequence.py
@@ -1,12 +1,12 @@
 """
 https://leetcode.com/problems/longest-arithmetic-subsequence/description/
-
 Given an array nums of integers, return the length of the longest arithmetic subsequence in nums.
 
 Note that:
 
 A subsequence is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements.
 A sequence seq is arithmetic if seq[i + 1] - seq[i] are all the same value (for 0 <= i < seq.length - 1).
+
  
 
 Example 1:
