60a96257277d020546865afda39b8980be2bcb21
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2025-11-03 13:40:37 +0530

    Update count_pallindromic_substring.py

--- ds/array/DP/palindromic_sub_sequence/count_pallindromic_substring.py
+++ ds/array/DP/palindromic_sub_sequence/count_pallindromic_substring.py
@@ -1,6 +1,29 @@
 """
 https://leetcode.com/problems/palindromic-substrings/
 
+Given a string s, return the number of palindromic substrings in it.
+
+A string is a palindrome when it reads the same backward as forward.
+
+A substring is a contiguous sequence of characters within the string.
+
+Example 1:
+
+Input: s = "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+Example 2:
+
+Input: s = "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+
+Note: 
+bruite force: at each i position expand search to left and righ side.
+case 1: even size pallindrom where l == r
+case 2: odd size pallindrom, where at middle there will be a common character and i-1 == i+1.
+
+DP: 
 """
 class Solution:
     def countSubstrings(self, s: str) -> int:
