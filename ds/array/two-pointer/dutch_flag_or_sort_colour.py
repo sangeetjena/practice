e923b7d3626bbc6f251941b21736eef559872f9c
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2025-11-05 14:33:12 +0530

    Update dutch_flag_or_sort_colour.py

--- ds/array/two-pointer/dutch_flag_or_sort_colour.py
+++ ds/array/two-pointer/dutch_flag_or_sort_colour.py
@@ -32,8 +32,8 @@ Follow up: Could you come up with a one-pass algorithm using only constant extra
 Note:
 approach1: use a dictionary count each color frequency and insert the element back to array, all same colour in single sort then next ...
 approch 2: all the 0 should go to left side , all 1 should stay in middle and all 2 will go to right side.
-always check mid value if it is 0 or 2 , then move the element else increase the m value.
-alwas check for m position and decide.
+always check mid value if it is 0 or 2 , think m as traffic manager, if 0 will come it will send to start pointer, if 2 will come it will send it to end pointer.
+and start ponter side size will increase so m will give space for 0 value by moving to next position,
 """
 
 
