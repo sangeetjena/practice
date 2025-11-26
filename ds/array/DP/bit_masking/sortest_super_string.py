40b3ca3570d2da95545781d5d2f1b0a9cb2c11a5
sangeet kumar jena <sangeet.kumar.jena@gmail.com>
2025-11-26 08:33:48 +0530

    Update sortest_super_string.py

--- ds/array/DP/bit_masking/sortest_super_string.py
+++ ds/array/DP/bit_masking/sortest_super_string.py
@@ -1,7 +1,8 @@
 """
 https://leetcode.com/problems/find-the-shortest-superstring/description/
 
-Given an array of strings words, return the smallest string that contains each string in words as a substring. If there are multiple valid strings of the smallest length, return any of them.
+Given an array of strings words, return the smallest string that contains each string in words as a substring. 
+If there are multiple valid strings of the smallest length, return any of them.
 
 You may assume that no string in words is a substring of another string in words.
 
